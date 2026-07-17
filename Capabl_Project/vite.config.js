70d6e1319e7d2b1feacd90ef4d3d1cf15236d1d1
swaraaghebbar <swaraag.hebbar@gmail.com>
2026-07-17 21:47:14 +0530

    changed url to render url

--- Capabl_Project/vite.config.js
+++ Capabl_Project/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://vytalcare-scy4.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
